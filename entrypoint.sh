11dff3f1c025ef521f501c0af44f3d5d51565e79
Jonahlyn <jgilstrap@gmail.com>
2024-04-29 20:59:22 -0600

    Work with image in container registry.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 # Run the fabric setup command
 yes '' | head -n 3 | fabric --setup
 
+cp -a ~/.config/custom-fabric-patterns/* ~/.config/fabric/patterns/
+
 # Run the main command
 if [ -z "$1" ]; then
     # Default command
